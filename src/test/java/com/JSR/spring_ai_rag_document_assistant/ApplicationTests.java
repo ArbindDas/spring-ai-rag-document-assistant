26b2bf0f1991f0a789e0235f4071745193daaca1
Arbind Das <dasarbind269@gmail.com>
2026-03-16 22:54:55 +0545

    first commit

---
+++ src/test/java/com/JSR/spring_ai_rag_document_assistant/ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.JSR.spring_ai_rag_document_assistant;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@Disabled
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
